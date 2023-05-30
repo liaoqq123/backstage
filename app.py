5e1f35766796f4c72dec28b85309e5178a09166e
stars <397801612@qq.com>
2023-05-31 01:08:06 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def open_login():
+    #打开登录页面
+    return render_template("main.html")
+
+@app.route('/login', methods=["post"])
+def login_data():
+    #返回登录数据
+    return request.form
+
+if __name__ == '__main__':
+    app.run()

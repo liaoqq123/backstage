05cf731f4cc93a65e902644017875cb73860fc5a
star <397801612@qq.com>
2023-06-02 19:54:57 +0800

    1

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request
-
+from user_data import user_data
 app = Flask(__name__)
 
 
 @app.route('/')
 def open_login():
     #打开登录页面
-    return render_template("Main.html")
+    return render_template("Login.html")
 
-@app.route('/login', methods=["post"])
+@app.route('/login', methods=["POST"])
 def login_data():
-    #返回登录数据
-    return request.form
+    #登录并获取用户数据
+    username = request.form.get('username')
+    password = request.form.get('password')
+    for user in user_data:
+        if user['username'] == username:
+            if user['password'] == password:
+                return render_template("Main.html")
+            else:
+                print('密码错误')
+        else:
+            print('用户名错误')
+    print(username, password)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ user_data.py
@@ -0,0 +1,4 @@
+user_data = [
+  {"username": "sb张益豪", "password": "dsb"},
+  {"username": "123456", "password": "111"}
+]
